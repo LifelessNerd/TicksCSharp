4c0040bece0f20d2ef0b37fd4ee93ad68df36530
LifelessNerd <lukadevrij@gmail.com>
2021-12-24 14:24:32 +0100

    Initial commit

---
+++ TicksCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicksCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Input the amount of ticks to be converted.");
+            int ticks = Int32.Parse(Console.ReadLine());
+            int secs = ticks / 20;
+            TimeSpan t = TimeSpan.FromSeconds(secs);
+
+            string answer = string.Format("{0:D2}d, {1:D2}h, {2:D2}m, {3:D2}s",
+                            t.Days,
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds);
+
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
